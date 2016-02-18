54da4c206ed510f95865b89aa9bc78e9e75fe207
Dan Loewenherz <dloewenherz@gmail.com>
2016-02-17 21:36:41 -0600

    add check

--- bump_version.sh
+++ bump_version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sed "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" LionheartExtensions.podspec
-git add .
-git commit -m "bump version to $1"
-git tag $1
+if [ "$1" != "" ]; then
+  sed "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" LionheartExtensions.podspec
+  git add .
+  git commit -m "bump version to $1"
+  git tag $1
+fi
