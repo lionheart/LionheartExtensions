63b33717cf987fb25fd16a9dc40612bcf74fb0d5
Dan Loewenherz <dloewenherz@gmail.com>
2016-02-17 21:12:02 -0600

    add generate docs script

---
+++ generate_docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+jazzy \
+  --author Lionheart \
+  --author_url http://lionheartsw.com \
+  --github_url https://github.com/lionheart/LionheartExtensions \
+  --github-file-prefix https://github.com/lionheart/LionheartExtensions/tree/0.1.0 \
+  --module-version 0.1.0 \
+  --module LionheartExtensions
