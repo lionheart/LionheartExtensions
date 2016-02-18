ab7cdb0525e16e2fc5ede3eb91949509a04c5dba
Dan Loewenherz <dloewenherz@gmail.com>
2016-02-17 21:38:39 -0600

    bump version to 0.1.1

--- bump_version.sh
+++ bump_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-  sed "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" LionheartExtensions.podspec
+  sed -i "" "s/\(s.version[ ]*=[ ]\).*/\1 \"$1\"/g" LionheartExtensions.podspec
   git add .
   git commit -m "bump version to $1"
   git tag $1
