3b04ac61b977f81c74480d6b3d23e94eae8bc344
Dan Loewenherz <dloewenherz@gmail.com>
2016-03-04 14:47:08 -0600

    update pod target support files

---
+++ Pods/Target Support Files/LionheartTableViewCells/LionheartTableViewCells-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LionheartTableViewCells : NSObject
+@end
+@implementation PodsDummy_LionheartTableViewCells
+@end
---
+++ Pods/Target Support Files/LionheartTableViewCells/LionheartTableViewCells-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LionheartTableViewCellsVersionNumber;
+FOUNDATION_EXPORT const unsigned char LionheartTableViewCellsVersionString[];
+
