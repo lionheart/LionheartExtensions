d64821c5559a412f4a01b34db3864c0658e589cd
Dan Loewenherz <dloewenherz@gmail.com>
2016-03-10 09:40:26 -0600

    reorganize project structure

---
+++ Example/Pods/Target Support Files/Pods-LionheartExtensions_Tests/Pods-LionheartExtensions_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LionheartExtensions_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LionheartExtensions_TestsVersionString[];
+
