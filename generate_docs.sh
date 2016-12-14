da20e2887e01d88838db78a0666b27749755adb1
Dan Loewenherz <dan@lionheartsw.com>
2016-12-13 19:32:35 -0600

    bump version to 3.0.2

--- generate_docs.sh
+++ generate_docs.sh
@@ -4,7 +4,7 @@ jazzy \
   --author Lionheart \
   --author_url http://lionheartsw.com \
   --github_url https://github.com/lionheart/LionheartExtensions \
-  --github-file-prefix https://github.com/lionheart/LionheartExtensions/tree/3.0.1 \
+  --github-file-prefix https://github.com/lionheart/LionheartExtensions/tree/3.0.2 \
   --module LionheartExtensions
 
 git co gh-pages
