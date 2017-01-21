1dc29c14e87b3b63141e317a78f8f179a38bf5c1
Dan Loewenherz <dan@lionheartsw.com>
2017-01-20 18:38:53 -0600

    another checkpoint commit

--- Pod/Classes/Core/NSLayoutAnchor+LionheartExtensions.swift
+++ Pod/Classes/Core/NSLayoutAnchor+LionheartExtensions.swift
@@ -18,96 +18,99 @@ infix operator ~~: ConstraintPrecedence
 infix operator ≥≥: ConstraintPrecedence
 infix operator ≤≤: ConstraintPrecedence
 
-public protocol HasAnchor {
-    associatedtype Anchor
+public protocol Anchoring {
+    associatedtype T
 
-    var constant: CGFloat { get }
-    var anchor: Anchor { get }
-}
-
-public protocol AxisAnchor: HasAnchor {
-    func constraint(equalTo: Anchor, constant: CGFloat) -> NSLayoutConstraint
-    func constraint(lessThanOrEqualTo: Anchor, constant: CGFloat) -> NSLayoutConstraint
-    func constraint(greaterThanOrEqualTo: Anchor, constant: CGFloat) -> NSLayoutConstraint
-}
-
-public protocol DimensionAnchor: HasAnchor {
     var multiplier: CGFloat { get }
+    var constant: CGFloat { get }
+    var anchor: T { get }
+}
 
+public protocol AxisAnchoring: Anchoring {
+    func constraint(equalTo: T, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(lessThanOrEqualTo: T, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(greaterThanOrEqualTo: T, constant: CGFloat) -> NSLayoutConstraint
+}
+
+public protocol DimensionAnchoring: Anchoring {
     func constraint(equalToConstant: CGFloat) -> NSLayoutConstraint
     func constraint(lessThanOrEqualToConstant: CGFloat) -> NSLayoutConstraint
     func constraint(greaterThanOrEqualToConstant: CGFloat) -> NSLayoutConstraint
 
-    func constraint(equalTo: Anchor, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
-    func constraint(lessThanOrEqualTo: Anchor, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
-    func constraint(greaterThanOrEqualTo: Anchor, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(equalTo: T, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(lessThanOrEqualTo: T, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
+    func constraint(greaterThanOrEqualTo: T, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint
 }
 
-public struct LayoutContainer<T>: HasAnchor {
-    public typealias Anchor = T
+public struct LayoutContainer<U>: Anchoring {
+    public typealias T = U
 
     public var multiplier: CGFloat
     public var constant: CGFloat
-    public var anchor: Anchor
+    public var anchor: T
 
-    init(anchor: Anchor, constant: CGFloat) {
+    init<U: Anchoring>(anchor: U, constant: CGFloat) where U.T == T {
         self.constant = constant
-        self.multiplier = 1
-        self.anchor = anchor
+        self.multiplier = anchor.multiplier
+        self.anchor = anchor.anchor
+    }
+
+    init<U: DimensionAnchoring>(anchor: U, multiplier: CGFloat) where U.T == T {
+        self.constant = anchor.constant
+        self.multiplier = multiplier
+        self.anchor = anchor.anchor
+    }
+
+    static func +(lhs: LayoutContainer<U>, rhs: CGFloat) -> LayoutContainer {
+        return LayoutContainer(anchor: lhs, constant: rhs)
+    }
+
+    static func -(lhs: LayoutContainer<U>, rhs: CGFloat) -> LayoutContainer {
+        return LayoutContainer(anchor: lhs, constant: -rhs)
     }
 }
 
-extension LayoutContainer where T: DimensionAnchor {
-    init(container: LayoutContainer, multiplier: Double) {
-        self.constant = container.constant
-        self.multiplier = CGFloat(multiplier)
-        self.anchor = container.anchor
-    }
-
-    static func *(lhs: LayoutContainer, rhs: Double) -> LayoutContainer {
-        return LayoutContainer(container: lhs, multiplier: rhs)
-    }
-}
-
-extension NSLayoutYAxisAnchor: AxisAnchor {
-    public typealias Anchor = NSLayoutAnchor<NSLayoutYAxisAnchor>
-
-    public var constant: CGFloat { return 0 }
-    public var anchor: Anchor { return self }
-}
-
-extension NSLayoutXAxisAnchor: AxisAnchor {
-    public typealias Anchor = NSLayoutAnchor<NSLayoutXAxisAnchor>
-
-    public var constant: CGFloat { return 0 }
-    public var anchor: Anchor { return self }
-}
-
-extension NSLayoutDimension: DimensionAnchor {
-    public typealias Anchor = NSLayoutDimension
+extension NSLayoutYAxisAnchor: AxisAnchoring {
+    public typealias T = NSLayoutAnchor<NSLayoutYAxisAnchor>
 
     public var multiplier: CGFloat { return 1 }
     public var constant: CGFloat { return 0 }
-    public var anchor: Anchor { return self }
+    public var anchor: T { return self }
 }
 
-public extension DimensionAnchor where Anchor: DimensionAnchor {
+extension NSLayoutXAxisAnchor: AxisAnchoring {
+    public typealias T = NSLayoutAnchor<NSLayoutXAxisAnchor>
+
+    public var multiplier: CGFloat { return 1 }
+    public var constant: CGFloat { return 0 }
+    public var anchor: T { return self }
+}
+
+extension NSLayoutDimension: DimensionAnchoring {
+    public typealias T = NSLayoutDimension
+
+    public var multiplier: CGFloat { return 1 }
+    public var constant: CGFloat { return 0 }
+    public var anchor: T { return self }
+}
+
+public extension DimensionAnchoring {
     @discardableResult
-    static func ~~<T: DimensionAnchor>(lhs: Self, rhs: T) -> NSLayoutConstraint where T.Anchor == Anchor {
+    static func ~~<U: Anchoring>(lhs: Self, rhs: U) -> NSLayoutConstraint where U.T == T {
         let constraint = lhs.constraint(equalTo: rhs.anchor, multiplier: rhs.multiplier, constant: rhs.constant)
         constraint.isActive = true
         return constraint
     }
 
     @discardableResult
-    static func ≤≤<T: DimensionAnchor>(lhs: Self, rhs: T) -> NSLayoutConstraint where T.Anchor == Anchor {
+    static func ≤≤<U: Anchoring>(lhs: Self, rhs: U) -> NSLayoutConstraint where U.T == T {
         let constraint = lhs.constraint(lessThanOrEqualTo: rhs.anchor, multiplier: rhs.multiplier, constant: rhs.constant)
         constraint.isActive = true
         return constraint
     }
 
     @discardableResult
-    static func ≥≥<T: DimensionAnchor>(lhs: Self, rhs: T) -> NSLayoutConstraint where T.Anchor == Anchor {
+    static func ≥≥<U: Anchoring>(lhs: Self, rhs: U) -> NSLayoutConstraint where U.T == T {
         let constraint = lhs.constraint(greaterThanOrEqualTo: rhs.anchor, multiplier: rhs.multiplier, constant: rhs.constant)
         constraint.isActive = true
         return constraint
@@ -133,37 +136,40 @@ public extension DimensionAnchor where Anchor: DimensionAnchor {
         constraint.isActive = true
         return constraint
     }
+
+    @discardableResult
+    static func *(lhs: Self, rhs: CGFloat) -> LayoutContainer<Self.T> {
+        return LayoutContainer(anchor: lhs, multiplier: rhs)
+    }
 }
 
-public extension AxisAnchor where Anchor: AxisAnchor {
+public extension AxisAnchoring {
     @discardableResult
-    static func ~~<T: AxisAnchor>(lhs: Self, rhs: T) -> NSLayoutConstraint where T.Anchor == Anchor {
+    static func ~~<U: Anchoring>(lhs: Self, rhs: U) -> NSLayoutConstraint where U.T == T {
         let constraint = lhs.constraint(equalTo: rhs.anchor, constant: rhs.constant)
         constraint.isActive = true
         return constraint
     }
 
     @discardableResult
-    static func ≤≤<T: AxisAnchor>(lhs: Self, rhs: T) -> NSLayoutConstraint where T.Anchor == Anchor {
+    static func ≤≤<U: Anchoring>(lhs: Self, rhs: U) -> NSLayoutConstraint where U.T == T {
         let constraint = lhs.constraint(lessThanOrEqualTo: rhs.anchor, constant: rhs.constant)
         constraint.isActive = true
         return constraint
     }
 
     @discardableResult
-    static func ≥≥<T: AxisAnchor>(lhs: Self, rhs: T) -> NSLayoutConstraint where T.Anchor == Anchor {
+    static func ≥≥<U: Anchoring>(lhs: Self, rhs: U) -> NSLayoutConstraint where U.T == T {
         let constraint = lhs.constraint(greaterThanOrEqualTo: rhs.anchor, constant: rhs.constant)
         constraint.isActive = true
         return constraint
     }
-}
 
-public extension HasAnchor {
-    static func +(lhs: Self, rhs: CGFloat) -> LayoutContainer<Anchor> {
-        return LayoutContainer(anchor: lhs.anchor, constant: rhs)
+    static func +(lhs: Self, rhs: CGFloat) -> LayoutContainer<T> {
+        return LayoutContainer(anchor: lhs, constant: rhs)
     }
 
-    static func -(lhs: Self, rhs: CGFloat) -> LayoutContainer<Anchor> {
-        return LayoutContainer(anchor: lhs.anchor, constant: -rhs)
+    static func -(lhs: Self, rhs: CGFloat) -> LayoutContainer<T> {
+        return LayoutContainer(anchor: lhs, constant: -rhs)
     }
 }
