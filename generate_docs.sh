b5c1097646733aa60af46c0ab796c02046424b50
Dan Loewenherz <dan@lionheartsw.com>
2017-05-23 20:08:02 -0500

    documentation update

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-jazzy --xcodebuild-arguments -workspace,Example/LionheartExtensions.xcworkspace,-scheme,LionheartExtensions
+jazzy
 
 git co gh-pages
 cp -r docs/* .
 rm -rf docs/
 git add .
 git commit -m "documentation update"
+git push origin gh-pages
 git co master
